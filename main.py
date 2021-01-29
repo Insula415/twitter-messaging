fbf196665f86d108c4b3d322c2eef139b60da141
Insula <66838181+Insula415@users.noreply.github.com>
2021-01-29 18:24:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -25,13 +25,17 @@ print("")
 confirm = input("Are you sure you want to send the following? ")
 opt = ["y", "yes"]
 
+
 if confirm.lower() in opt:
     for i in h:
         try:
-            direct_message = api.send_direct_message(i.strip(), text) #sending message
-            print("Sent to",i)
+            user = api.get_user(i)
+            id = user.id_str
+            direct_message = api.send_direct_message(id.strip(), text) #sending message
+            u = api.get_user(id)
+            print("Sent to",u.screen_name)
         except:
-            print("Failed to send to",i)
+            print("Failed to send to",user.strip())
 else:
     print("Cancelled")
     exit()
