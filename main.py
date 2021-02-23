5b8effc83521c57cf7fa0c2a99918219b0b95072
Insula <66838181+Insula415@users.noreply.github.com>
2021-02-23 11:18:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tweepy
 import sys
-import time
+from time import sleep
+from collections import Counter, defaultdict
 
 consumer_key = "" #your consumer key
 consumer_secret = "" #your consumer secret key
@@ -12,6 +13,31 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+
+print("Checking for duplicates")
+print("")
+count = defaultdict(int)
+d = 0
+with open('handles.txt') as f:
+    l = Counter(l.strip() for l in f if l.strip())
+    for line in l:
+        if l[line]>1:
+            print(line)
+
+with open('handles.txt') as f:
+    for lineno, line in enumerate(f):
+        c = line.strip()
+        if c:
+            if c in count:
+                print("Line:",lineno)
+                d += 1
+            count[c] += 1
+
+if d >= 1:
+    print("Duplicates found:",d)
+    exit()
+else:
+    print("No duplicates found, all ready to go!")
 
 
 f = open("handles.txt", "r")
@@ -26,7 +52,15 @@ confirm = input("Are you sure you want to send the following? ")
 opt = ["y", "yes"]
 
 
+x = 0
 if confirm.lower() in opt:
+    print("Sending in 3")
+    sleep(1)
+    print("Sending in 2")
+    sleep(1)
+    print("Sending in 1")
+    sleep(1)
+    print("Blast off")
     for i in h:
         try:
             user = api.get_user(i)
@@ -34,8 +68,12 @@ if confirm.lower() in opt:
             direct_message = api.send_direct_message(id.strip(), text) #sending message
             u = api.get_user(id)
             print("Sent to",u.screen_name)
+            x += 1
+
         except:
-            print("Failed to send to",user.strip())
+            print("Failed to send to",u.screen_name)
 else:
     print("Cancelled")
     exit()
+
+print("Successfully sent to",x,"people")
